3f1422a2beab1f79638bd9fe3616d8aaad0c4a5c
Brielle McCormick <briellemccormick@Brielles-MBP.fios-router.home>
2020-07-20 20:09:52 -0400

    adjusted readme formatting and added more buttons

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,37 +2,36 @@ function generateMarkdown(markdown) {
   return `
 
 ![GitHub repo size](https://img.shields.io/github/repo-size/${markdown.username}/${markdown.repoName})
+![GitHub stars](https://img.shields.io/github/stars/${markdown.username}/${markdown.repoName}?style=social)
+![GitHub forks](https://img.shields.io/github/forks/${markdown.username}/${markdown.repoName}?style=social)
 
 # ${markdown.repoName}
+Created by ${markdown.username}
 
 ![Github icon image](https://avatars1.githubusercontent.com/u/64443618?v=4)
 
-Created by ${markdown.username}
-
-![Github icon image]
-${markdown.icon}
-
-# CONTACT
-${markdown.userEmail}
-
-# DESCRIPTION
+## Description
 ${markdown.description}
 
-# USAGE
+## Usage
 ${markdown.usage}
 
-# INSTALLATION 
+## Installation
 ${markdown.installation}
 
-# LICENSE
+## License
 ${markdown.license}
 
-# Interested in contributing? Here's how:
+## Interested in contributing? Here's how:
 ${markdown.contributing}
 
-# TESTING
+## Testing
 ${markdown.tests}
 
+## Questions 
+
+## Contact
+For more information, please email me at ${markdown.userEmail}
 
 `;
 }
@@ -49,114 +48,3 @@ ${markdown.tests}
 //     icon: "",
 
 module.exports = generateMarkdown;
-
-// new Promise((resolve, reject) => {
-//   console.log("Initial");
-
-//   resolve();
-// })
-//   .then(() => {
-//     throw new Error("Something failed");
-
-//     console.log("Do this");
-//   })
-//   .catch(() => {
-//     console.error("Do that");
-//   })
-//   .then(() => {
-//     console.log("Do this, no matter what happened before");
-//   });
-
-// let promise = new Promise((resolve, reject) => {
-//   inquirer.prompt(questions);
-// }).then(function ({ username, userEmail }) {
-//   const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-
-//   axios.get(queryUrl).then(({ data }) => {
-//     const reposArr = data.map((obj) => obj.name);
-//     const repoStr = reposArr.join("\n");
-//     console.log("Repositories found:" + "\n" + repoStr);
-//     return repoStr;
-//   });
-//   resolve();
-//   console.log("done");
-// });
-// promise.then(function () {
-//   inquirer.prompt({
-//     message: "Please enter your repo name:",
-//     name: "repoName",
-//   })
-//   .then(function ({ repoName}) {
-//   userObj = {
-//     username: username,
-//     userEmail: userEmail,
-//   };
-// });
-
-// if (repoArr[0].description === null) {
-//   inquirer
-//     .prompt({
-//       message: "Enter a project description:",
-//       name: "projectDesc",
-//     })
-//     .then(function (projectDesc) {
-//       markdown.description = projectDesc;
-//     });
-// } else if ((markdown.description = repoArr[0].description));
-
-// //////////////////////////
-// const markdown,description =  if (repoArr[0].description !== null) {
-//               markdown.description = repoArr[0].description;
-//               console.log(markdown);
-//             } else {
-//               inquirer
-//                 .prompt({
-//                   message: "Enter a project description:",
-//                   name: "projectDesc",
-//                 })
-
-// inquirer
-//   .prompt(questions)
-//   .then(function (answers) {
-//     console.log(answers);
-//   })
-
-//   .then(function ({ username, userEmail }) {
-//     markdown.username = username;
-//     markdown.userEmail = userEmail;
-//     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-
-//     axios.get(queryUrl).then(({ data }) => {
-//       const reposArr = data.map((obj) => obj.name);
-//       const repoStr = reposArr.join("\n");
-//       console.log("Repositories found:" + "\n" + repoStr);
-
-//       inquirer
-//         .prompt({
-//           message: "Enter your repo name:",
-//           name: "repoName",
-//         })
-
-// .then(function ({ repoName }) {
-//   markdown.repoName = repoName;
-//   const repoArr = data.filter(
-//     (data) => data.name.toLowerCase() == repoName.toLowerCase()
-//   );
-
-//         if (repoArr[0].description !== null) {
-//           markdown.description = repoArr[0].description;
-//           console.log(markdown);
-//         } else {
-//           inquirer
-//             .prompt({
-//               message: "Enter a project description:",
-//               name: "projectDesc",
-//             })
-//             .then(function ({ projectDesc }) {
-//               markdown.description = projectDesc;
-//               console.log(markdown);
-//             });
-//         }
-//       });
-//   });
-// });
